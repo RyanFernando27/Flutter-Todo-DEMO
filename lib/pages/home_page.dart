4d1474860185d113a3f15d6065503c51ceaffbd6
RyanFernando27 <138043611+RyanFernando27@users.noreply.github.com>
2025-01-11 20:55:04 +0530

    end project

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,6 +9,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _controller = TextEditingController();
+
   List todoList = [
     ['Learn Flutter', true],
     ['OOP exam', true],
@@ -28,9 +30,49 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  void addNewTask() {
+    setState(() {
+      todoList.add([_controller.text, false]);
+      _controller.clear();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: TextField(
+                  controller: _controller,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      hintText: 'Add a new Task',
+                      filled: true,
+                      fillColor: const Color.fromARGB(255, 237, 237, 237)),
+                ),
+              ),
+            ),
+            FloatingActionButton(
+              tooltip: 'Add',
+              elevation: 0,
+              backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+              onPressed: addNewTask,
+              child: const Icon(
+                Icons.add,
+                color: Colors.white,
+              ),
+            )
+          ],
+        ),
+      ),
       backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text('Todo App'),
